4e1f28100cc297abae9c198778ca343414a72f9b
Henrihva Alves <henrihva@gmail.com>
2023-07-27 23:00:12 +0000

    Adcionado AppTeste

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,11 +8,11 @@ public class App {
     private static final String PASSWORD = "";
     private static final String USERNAME = "gitpod";
     private static final String JDBC_URL = "jdbc:postgresql://localhost/postgres";
-
     public static void main(String[] args) {
         new App();
     }
 
+    // teste 
     public App(){
         try(var conn = getConnection()){
             carregarDriverJDBC();
---
+++ src/main/java/com/example/AppTeste.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class AppTeste {
+
+    public static void main(String[] args) {
+        System.out.println("App de Teste");
+    }
+    
+}
