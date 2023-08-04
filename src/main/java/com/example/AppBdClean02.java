536f2dc95b9614765134e257171a2c80b6122b70
Henrihva Alves <henrihva@gmail.com>
2023-08-04 00:01:11 +0000

    CLeanCode AppBdClean02

--- src/main/java/com/example/AppBdClean02.java
+++ src/main/java/com/example/AppBdClean02.java
@@ -3,26 +3,54 @@ package com.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-//import java.sql.Statement;
 
-public class AppBdClean {
+public class AppBdClean02 {
+    // parametros drive:banco://endereço/nome do banco, usuario , senha 
     private static final String PASSWORD = "";
     private static final String USERNAME = "gitpod";
     private static final String JDBC_URL = "jdbc:postgresql://localhost/postgres";
-    // private Connection conn; 
     public static void main(String[] args) {
-        new AppBdClean();
-
+        new AppBdClean02();
     }
 
     // criando um construtor pois tirou o static dos metodos
-    public AppBdClean(){
-        //CarregarDriverJDBC();
+    public AppBdClean02(){
         try (var conn = getConnection()){
-            listarEstados(conn); 
-            localizarEstado(conn, "TO");
+            carregarDriverJDBC();
+            // listarEstados(conn); 
+            // localizarEstado(conn, "TO");
+            // listarDadosTabela(conn, "cliente");
+            listarDadosTabela(conn, "produto");
         } catch (SQLException e) {
                 System.err.println("Não foi possível conectar ao banco de dados:  " + e.getMessage());
+        }
+    }
+
+    private void listarDadosTabela(Connection conn, String tabela) {
+        // neste caso o usuario não vai digitar o nome da tabela = variavel 
+        // se for em campo digitado, vc dentro da aplicação tem que limpar os caracteres que não quer 
+        var sql = "select * from "  + tabela;    
+        System.out.println(sql);
+        try {
+            var statement = conn.createStatement();
+            var result = statement.executeQuery(sql);
+
+            var metadada = result.getMetaData();
+            int cols = metadada.getColumnCount();
+
+            for (int i = 1; i <= cols; i++) {
+                System.out.printf("%-30s ! ", metadada.getColumnName(i));
+            }
+            System.out.println();
+            
+            while(result.next()) {
+                for (int i = 1; i <= cols; i++) {
+                    System.out.printf("%-30s ! ", result.getString(i));
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro na execução da consulta " + e.getMessage());
         }
 
     }
@@ -40,16 +68,10 @@ public class AppBdClean {
         } catch (SQLException e) {
             System.err.println("Não foi possível executar a consulta no banco de dados:  " + e.getMessage());
         }
-
-
     }
 
     private void listarEstados(Connection conn) {
-            
-//        Statement statement = null;
-//        try (var conn = getConnection()){
         try {
-            // parametros 1o= o drive:banco://endereço/nomedo banco, usuario , senha 
             System.out.println("COnexão com o banco realizada com sucesso!");
             
             var statement = conn.createStatement();
@@ -65,7 +87,7 @@ public class AppBdClean {
     private Connection getConnection () throws SQLException{
         return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
     }
-    private void CarregarDriverJDBC() {
+    private void carregarDriverJDBC() {
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
